664033d7a4d6feb859f15aa04e5abd1c1df19beb
TaylorTS <taylorts1021@gmail.com>
2023-06-14 17:35:17 -0500

    Update employment_data.sql

--- employment_data.sql
+++ employment_data.sql
@@ -1,16 +1,16 @@
 /*find the avg salary of the male and female employees in each department*/
 with cte as (
-	select a.emp_no, b.dept_name
-    from dept_emp as a
-    join departments as b
-    on a.dept_no = b.dept_no)
+	select emp.emp_no, d.dept_name
+    from dept_emp as emp
+    join departments as d
+    on emp.dept_no = d.dept_no)
 
 select cte.dept_name, c.gender, avg(c.salary) as 'average_salary'
 from cte
 join (select a.emp_no, a.salary, b.gender
-		from salaries as a
-		join employees as b
-		on a.emp_no = b.emp_no) as c
+		from salaries as s
+		join employees as e
+		on s.emp_no = e.emp_no) as c
 on cte.emp_no = c.emp_no
 group by c.gender, cte.dept_name
 order by cte.dept_name, c.gender
@@ -26,20 +26,20 @@ from employees
 where left(hire_date, 4) = '2000'
 limit 10;
 
-select a.emp_no, a.first_name, a.last_name, b.title
-from employees as a
-join titles as b
-on a.emp_no = b.emp_no
-where b.title like '%Engineer%'
+select e.emp_no, e.first_name, e.last_name, t.title
+from employees as e
+join titles as t
+on e.emp_no = t.emp_no
+where t.title like '%Engineer%'
 limit 10;
 
 select count(1) as 'number of engineers'
-from employees as a
-join titles as b
-on a.emp_no = b.emp_no;
+from employees as e
+join titles as t
+on e.emp_no = t.emp_no;
 
-select b.title, count(1) as 'number'
-from employees as a
-join titles as b
-on a.emp_no = b.emp_no
-group by b.title;
+select t.title, count(1) as 'number'
+from employees as e
+join titles as t
+on e.emp_no = t.emp_no
+group by t.title;
